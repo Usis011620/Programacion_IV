829db8ca0913e113cb18487a66ab6fb2f67c54a2
wilbert <ramirezwilbert39@gmail.com>
2022-03-03 16:52:48 -0600

    Se modificaron los doc js

--- progra/main.js
+++ progra/main.js
@@ -1,20 +1,63 @@
-var app = new Vue({
-    el: '#appCliente',
+var generarIdUnicoFecha = ()=>{
+    let fecha = new Date();
+    return Math.floor(fecha.getTime()/1000).toString(16);
+}, db;
+var appSistema = new Vue({
+    el: '#appSistema',
     data: {
-        cliente: {
-            accion: '',
-            msg : '',
-            idCliente: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            dui: ''
-        },
+        forms:{
+            'cliente':{mostrar:false},
+            'producto':{mostrar:false},
+            'proveedor':{mostrar:false},
+            'categoria':{mostrar:false}
+        }
     },
     methods: {
-        guardarCliente(){
-            console.log(this.cliente);
-        }
+        abrirBd(){
+            /**
+             * Mecanismos de Almacenamiento
+             * 1. WebSQL
+             * 2. localStorage
+             * 3. IndexedDB
+             */
+            let indexDb = indexedDB.open('db_sistema', 1);
+            indexDb.onupgradeneeded = e=>{
+                let db = e.target.result;
+                tblcliente = db.createObjectStore('cliente', {keyPath:'idCliente'});
+                tblproducto = db.createObjectStore('producto', {keyPath:'idProducto'});
+                tblproveedor = db.createObjectStore('proveedor', {keyPath:'idProveedor'});
+                tblcategoria = db.createObjectStore('categoria', {keyPath:'idCategoria'});
+
+                tblcliente.createIndex('idCliente', 'idCliente', {unique:true});
+                tblcliente.createIndex('codigo', 'codigo', {unique:false});
+
+                tblproducto.createIndex('idProducto', 'idProducto', {unique:true});
+                tblproducto.createIndex('codigo', 'codigo', {unique:false});
+
+                tblcategoria.createIndex('idCategoria', 'idCategoria', {unique:true});
+                tblcategoria.createIndex('codigo', 'codigo', {unique:false});
+            };
+            indexDb.onsuccess = e=>{
+                db = e.target.result;
+            };
+            indexDb.onerror = e=>{
+                console.log(e.target.error);
+            };
+        },
+    },
+    created(){
+        this.abrirBd();
     }
+});
+function abrirStore(store, modo){
+    return db.transaction(store, modo).objectStore(store);
+}
+document.addEventListener('DOMContentLoaded', e=>{
+    let formularios = document.querySelectorAll('.mostrar').forEach(formulario=>{
+        formulario.addEventListener('click', evento=>{
+            let formulario = evento.target.dataset.form;
+            appSistema.forms[formulario].mostrar = true;
+            appSistema.$refs[formulario].obtenerDatos();
+        });
+    });
 });
